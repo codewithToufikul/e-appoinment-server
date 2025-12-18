c9ef46bec197e81c50419e5084a0bafcfb70022d
codewithToufikul <imranmeg2356@gmail.com>
2025-12-19 00:36:13 +0600

    Configure for Vercel

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get('/api/health', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+      console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+    });
+}
+
+export default app;
